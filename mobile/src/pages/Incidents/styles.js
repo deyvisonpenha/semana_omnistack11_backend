f434871055607384875827bea8a3a6d2b94c3620
Deyvison Penha <deyvisonpenha1@gmail.com>
2020-04-04 20:36:27 +0000

    commit mobile

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -25,5 +25,45 @@ export default StyleSheet.create({
         marginBottom: 16,
         marginTop: 48,
         color: '#13131a'
+    },
+    description: {
+        fontSize:16,
+        lineHeight: 24,
+        color: '#737380'
+    },
+    incidentList: {
+        marginTop: 32,
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        margingBottom: 16,
+    },
+
+    incidentProperty: {
+        fontSize:14,
+        color: '#41414',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    detailsButtonText: {
+        color: '#e02041',
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 })  
